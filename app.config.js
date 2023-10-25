503ab9c5df57e08c9e8116aae7df5bfd07071bfd
Cumhurcan <cumhurcangs97@gmail.com>
2023-10-25 23:38:35 +0300

    build

---
+++ app.config.js
@@ -0,0 +1,23 @@
+export default {
+  expo: {
+    extra: {
+      eas: {
+        projectId: "35d5add0-9253-449c-93cc-ec4f235dafde",
+      },
+    },
+    android: {
+      package: "com.ccankaya.restostaring",
+      config: {
+        appSecret: {
+          value: '"c2b948c9-db92-43df-8455-7e9204eec35e"',
+        },
+      },
+    },
+    ios: {
+      bundleIdentifier: "com.your.bundle.identifier",
+      config: {
+        appSecret: '"c2b948c9-db92-43df-8455-7e9204eec35e"',
+      },
+    },
+  },
+};
